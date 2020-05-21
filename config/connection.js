8abcfe85bb5794f63c792fbf3437101e21e778f2
Mwansa <Mwansad.mwango@gmail.com>
2020-05-21 22:17:12 +0800

    Updated folder structure so that package.json is in root folder

---
+++ config/connection.js
@@ -0,0 +1,29 @@
+// Set up MySQL connection.
+const mysql = require("mysql");
+
+
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	const connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'sql0044',
+		database: 'burgers_db'
+	})
+};
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
